0e3f38036b747535532648443b2508c1d1b5a9ab
Ritesh Patel <patelritesh261@outlook.com>
2016-07-07 13:51:17 -0400

    Add project files.

---
+++ COMP2007-S2016-LESSION8B/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace COMP2007_S2016_LESSION8B
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ COMP2007-S2016-LESSION8B/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP2007_S2016_LESSION8B.MvcApplication" Language="C#" %>
